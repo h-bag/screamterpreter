318732164d6c1116a38e7636f87786f7d3d63cb7
Baguette <30416854+H-Baguette@users.noreply.github.com>
2020-10-01 16:31:00 +0100

    Add loading from files
    Adds loading from files, as well as a few UI changes.

--- screamterpreter.py
+++ screamterpreter.py
@@ -1,3 +1,5 @@
+import os
+
 def getch():
     try:
         import msvcrt
@@ -8,11 +10,12 @@ def getch():
 
 
 def interpret(scream):
+    print("---------------------------------------\nOutput:\n")
     pointerMap = {0:0}
     pointerLocation = 0
     step = 0
     code = scream.split(" ")
-    while step < (len(code)-1):
+    while step < (len(code)):
         if code[step] == 'AAAH':
             pointerLocation += 1
         elif code[step] == 'AAAAGH':
@@ -26,13 +29,11 @@ def interpret(scream):
                 pointerMap[pointerLocation] = 0
             pointerMap[pointerLocation] -= 1
         elif code[step] == '!!!!!!':
-            print(chr(pointerMap[pointerLocation]),end='')
+            print(chr(pointerMap[pointerLocation]),end='',flush=True)
         elif code[step] == 'WHAT?!':
             charInput = getch()
-            try:
-                pointerMap[pointerLocation] = int(charInput)
-            except ValueError:
-                pointerMap[pointerLocation] = ord(charInput)
+            
+            pointerMap[pointerLocation] = ord(charInput)
         elif code[step] == 'OW':
             if pointerMap[pointerLocation] == 0:
                 openBraces = 1
@@ -53,7 +54,50 @@ def interpret(scream):
             step -= 1
         step += 1
 
-print("Written by Baguette (https://hbaguette.neocities.org)")
-screamcode = input("Enter the SCREAMCODE to interpret below:\n---------------------------------------\n")
-print("---------------------------------------\nOutput:\n")
-interpret(screamcode)
+
+def writeDirect():
+    lines = []
+    inputFinished = False
+    inputStep = 1
+    print("\n\n\nTo finish your input, put a single . ALONE on a new line.")
+    print("Enter the SCREAMCODE to interpret below:\n---------------------------------------")
+    while inputFinished == False:
+        screamcode = input()
+        inputStep += 1
+        if screamcode == '.':
+            inputFinished = True
+        else:
+            lines.append(screamcode)
+
+    screamcode = ' '.join(lines)
+    interpret(' '.join(lines))
+
+def loadFile():
+    fileDir = os.getcwd() + '/screams/'
+    fileDirContents = os.listdir(fileDir)
+    print("\nFiles in screams directory:\n---------------------------")
+    print(*fileDirContents, sep='\n')
+    loadTarget = input("\nFile to load: ")
+    loadTarget = fileDir + loadTarget
+
+    try:
+        with open(loadTarget, encoding="utf-8") as file:
+            lines = [l.rstrip('\n') for l in file]
+        interpret(' '.join(lines))
+    except FileNotFoundError:
+        print("No such file in screams directory.")
+
+
+print('\n\nWritten by Baguette (https://hbaguette.neocities.org)')
+print('#####################################################\n## [L] Load File  [D] Interpret Directly  [Q] Quit ##\n#####################################################')
+menuSelected = False
+while menuSelected == False:
+    menuChoice = getch()
+    if menuChoice.lower() == b'l':
+        menuSelected = True
+        loadFile()
+    elif menuChoice.lower() == b'd':
+        menuSelected = True
+        writeDirect()
+    elif menuChoice.lower() == b'q':
+        quit()
